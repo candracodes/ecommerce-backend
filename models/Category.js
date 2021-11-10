f19f882d460852c27ac2aebd6e3ddc727f8d12a8
Candra Fried <candracodes@gmail.com>
2021-11-09 20:25:06 -0600

    Adding a few comments on Models in order to figure out how to break this project down into doable pieces.

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+Category.init({
+  // define columns
+
+  //id
+  // Integer.
+  // Doesn't allow null values.
+  // Set as primary key.
+  // Uses auto increment.
+
+  // category_name
+  // String.
+  // Doesn't allow null values.
+  
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'category',
+});
 
 module.exports = Category;
